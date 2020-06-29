60863ab85df30c946305471b8adf0c909cb0e679
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-06-29 17:43:32 +0200

    Remove further typos in the test function

--- nomenclature/tests/test_validate.py
+++ nomenclature/tests/test_validate.py
@@ -18,11 +18,11 @@ def test_validate():
 def test_validate_fail():
     # test that simple validation fails on variable and region dimension
     df = IamDataFrame(TEST_DF)
-    assert not validate(df.rename(variable={'Primary Energy': 'foo'}))
-    assert not validate(df.rename(region={'Europe': 'foo'}))
+    assert not (validate(df.rename(variable={'Primary Energy':'foo'})))
+    assert not (validate(df.rename(region={'Europe':'foo'})))
 
 
 def test_validate_directional():
     # test that validation works as expected with directional data
-    assert validate(IamDataFrame(TEST_DF).rename(region={'Europe': 'Austria>Germany'}))
-    assert not validate(IamDataFrame(TEST_DF).rename(region={'Europe': 'Austria>foo'}))
+    assert validate(IamDataFrame(TEST_DF).rename(region={'Europe':'Austria>Germany'}))
+    assert not validate(IamDataFrame(TEST_DF).rename(region={'Europe':'Austria>foo'}))
