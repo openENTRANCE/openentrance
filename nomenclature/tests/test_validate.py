13b06204e561fd04f8fa8eb54445e7f59d062c75
sebastianzwickl <64072650+sebastianzwickl@users.noreply.github.com>
2020-07-09 05:56:33 +0200

    Fix validation fails on directional data  (#62)

---
+++ nomenclature/tests/test_validate.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from pyam import IamDataFrame
+from nomenclature import validate
+
+
+TEST_DF = pd.DataFrame([
+    ['model_a', 'scen_a', 'Europe', 'Primary Energy', 'EJ/yr', 1, 6.],
+],
+    columns=['model', 'scenario', 'region', 'variable', 'unit', 2005, 2010]
+)
+
+
+df = IamDataFrame(TEST_DF)
+
+
+def test_validate():
+    # test simple validation
+    assert validate(df)
+
+
+def test_validate_fail():
+    # test that simple validation fails on variable and region dimension
+    assert not (validate(df.rename(variable={'Primary Energy': 'foo'})))
+    assert not (validate(df.rename(region={'Europe': 'foo'})))
+
+
+def test_validate_directional():
+    # test that validation works as expected with directional data
+    assert validate(df.rename(region={'Europe': 'Austria>Germany'}))
+    assert not validate(df.rename(region={'Europe': 'Austria>foo'}))
+
+    # test that directional data with more than one `>` fails
+    assert not validate(df.rename(region={'Europe': 'Austria>Italy>France'}))
