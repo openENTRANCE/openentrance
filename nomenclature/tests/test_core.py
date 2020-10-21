9d88c312c852e329e4e71e194ef42d1b1d59eb68
Erik <erikfilias@gmail.com>
2020-10-21 11:19:13 +0200

    Fixing typos

--- nomenclature/tests/test_core.py
+++ nomenclature/tests/test_core.py
@@ -22,14 +22,14 @@ def test_variables_fuel_types():
 def test_variables_industry_types():
     # check that exploding of <industry> to industries works
     obs = nc.variables['Capital|iAGRI']
-    exp = 'Total capital costs spend by Agriculture'
+    exp = 'Total capital costs spend by agriculture'
     assert obs['description'] == exp
 
 
 def test_variables_product_types():
     # check that exploding of <product> to procuts works
     obs = nc.variables['Consumption|Households|pAGRI|Imported']
-    exp = 'Consumption of imported Agriculture by ' \
+    exp = 'Consumption of imported agriculture by ' \
           'households'
     assert obs['description'] == exp
 
