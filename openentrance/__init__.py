109ff76ae8124a2e6b6b775fc78bf809dd2084dd
Philip Hackstock <p.hackstock@gmx.at>
2022-11-16 08:15:46 +0100

    Change name to area_name (#234)
    * Change name to area_name
    
    * Update to use new nomenclature
    
    * Update tests to use new nomenclature
    
    * Adjust iso2_alt for new nomenclature

--- openentrance/__init__.py
+++ openentrance/__init__.py
@@ -1,22 +1,26 @@
 from pathlib import Path
 
-from nomenclature import CodeList
+from nomenclature.codelist import RegionCodeList
 
 
 # path to nomenclature definitions
 DEF_PATH = Path(__file__).parent.parent / "definitions" / "region"
 
 
-countries = CodeList.from_directory(
-    "region", path=DEF_PATH, file="countries.yaml"
+countries = RegionCodeList.from_directory(
+    "region", path=DEF_PATH, file_glob_pattern="countries.yaml"
 )
 """CodeList of countries"""
 
 iso_mapping = dict(
-    [(countries[c]["iso3"], c) for c in countries]
-    + [(countries[c]["iso2"], c) for c in countries]
+    [(countries[c].iso3, c) for c in countries]
+    + [(countries[c].iso2, c) for c in countries]
     # add alternative iso2 codes used by the European Commission to the mapping
-    + [(countries[c]["iso2_alt"], c) for c in countries if "iso2_alt" in countries[c]]
+    + [
+        (countries[c].iso2_alt, c)
+        for c in countries
+        if hasattr(countries[c], "iso2_alt")
+    ]
 )
 """Dictionary of iso2/iso3/alternative-iso2 codes to country names"""
 
@@ -34,10 +38,12 @@ def _create_nuts_hierarchy():
     """Parse nuts3.yaml and create hierarchical dictionary"""
 
     hierarchy = dict()
-    nuts3 = CodeList.from_directory("region", path=DEF_PATH, file="nuts3.yaml")
+    nuts3 = RegionCodeList.from_directory(
+        "region", path=DEF_PATH, file_glob_pattern="nuts3.yaml"
+    )
 
-    for n3, mapping in nuts3.items():
-        country, n1, n2 = [mapping.get(i) for i in ["country", "nuts1", "nuts2"]]
+    for n3, code in nuts3.items():
+        country, n1, n2 = [getattr(code, i) for i in ["country", "nuts1", "nuts2"]]
         country_dict = _add_to(hierarchy, country, {n1: dict()})
         n1_dict = _add_to(country_dict, n1, {n2: list()})
         _add_to(n1_dict, n2, [n3])
