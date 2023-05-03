627c2053d78d8f6f6b03e34c80a66621c59fedb7
Daniel Huppmann <dh@dergelbesalon.at>
2023-05-03 13:38:03 +0200

    Refactor to use nomenclature v0.9 (#274)
    * Remove explicit requirements
    
    * Refactor RegionProcessor signature, use nomenclature v0.9 or higher
    
    * Install pytest explicitly
    
    * Make "tests" an installation option

--- workflow.py
+++ workflow.py
@@ -31,7 +31,7 @@ def main(df: pyam.IamDataFrame, dimensions=["region", "variable"]) -> pyam.IamDa
     definition = DataStructureDefinition(
         here / "definitions", dimensions=dsd_dimensions
     )
-    processor = RegionProcessor.from_directory(path=here / "mappings")
+    processor = RegionProcessor.from_directory(here / "mappings", definition)
 
     # check if directional data exists in the scenario data, add to region codelist
     if any([r for r in df.region if ">" in r]):
